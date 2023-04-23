4aa6745da02543c3615007ef4b41d0aec14a0b7b
johnnybegood1318 <baldwinjr.j@gmail.com>
2023-04-23 06:00:11 +0000

    Create inital ROS2 packages and nodes

---
+++ install/RobotDriver/lib/python3.8/site-packages/RobotDriver/Driver.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from RobotDriver.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/Locator/Locator/HumanLocator.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from Locator.')
+
+
+if __name__ == '__main__':
+    main()
