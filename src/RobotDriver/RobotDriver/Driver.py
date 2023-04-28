39f52ea27a1e6b4ca3e2d521ccb0cd4ca6dec518
Severo Morales <severomorales99@gmail.com>
2023-04-28 12:41:54 -0400

    Better implementation

--- src/RobotDriver/RobotDriver/Driver.py
+++ src/RobotDriver/RobotDriver/Driver.py
@@ -7,43 +7,54 @@ from std_msgs.msg import String
 class Driver(Node):
     def __init__(self):
         super().__init__('driver')
-        self.publisher = self.create_publisher(Twist,'cmd_vel',0)
-        base_cmd = Twist()
+        self.publisher = self.create_publisher(Twist,'cmd_vel',10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.callback)
+        self.base_cmd = Twist()
 
-        base_cmd.linear.x = 100.0;
-        base_cmd.linear.y = 0.0;
-        base_cmd.linear.z = 0.0
-        base_cmd.angular.x = 100.0;
-        base_cmd.angular.y = 0.0;
-        base_cmd.angular.z = 0.0
-        print("Hello World")
-        self.publisher.publish(base_cmd)
+        self.base_cmd.linear.x = 0.0
+        self.base_cmd.linear.y = 0.0
+        self.base_cmd.linear.z = 0.0
+        self.base_cmd.angular.x = 0.0
+        self.base_cmd.angular.y = 0.0
+        self.base_cmd.angular.z = 0.0
+    
+    def update(self, linear_x , angular_z):
+        self.base_cmd.linear.x = linear_x
+        self.base_cmd.angular.z = angular_z
+        self.publisher.publish(self.base_cmd)
+    
+    def callback(self):
+        while(1):
+            self.update(5,5)
+
 
 
 
 
 def main(args=None):
-    #rclpy.init(args=args)
-    #driver = Driver()
-    #rclpy.spin(driver)
+    rclpy.init(args=args)
+    driver = Driver()
+    
+    rclpy.spin(driver)
 
-    #driver.destroy_node()
-    #rclpy.shutdown()
+    driver.destroy_node()
+    rclpy.shutdown()
     
     rclpy.init(args=args)
     node = rclpy.create_node('myNode')
     
-    pub = node.create_publisher(Twist, 'cmd_vel',10)
-    while(1):
-        twist = Twist()
-        twist.linear.x = 5.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 5.0
-        print("Hello World!")
-        pub.publish(twist)
+    #pub = node.create_publisher(Twist, 'cmd_vel',10)
+    #while(1):
+    #    twist = Twist()
+    #    twist.linear.x = 5.0
+    #    twist.linear.y = 0.0
+    #    twist.linear.z = 0.0
+    #    twist.angular.x = 0.0
+    #    twist.angular.y = 0.0
+    #    twist.angular.z = 5.0
+    #    print("Hello World!")
+    #    pub.publish(twist)
 
 if __name__ == '__main__':
     main()
